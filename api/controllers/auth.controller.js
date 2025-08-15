9eb3ca9859bf17281ddfa90035d5994c867cb519
vivekds19 <dsvivek092@gmail.com>
2025-08-15 20:53:01 +0530

    complete sigin page with redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import User from "../models/User.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
-import req from "express/lib/request.js";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,7 +10,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User created Succesfully");
+    res.status(201).json('User created successfully!');
   } catch (error) {
     next(error);
   }
@@ -20,14 +20,13 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User doesn't found !"));
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
-
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: pass, ...rest} = validUser._doc;
-        res
-      .cookie("access_token", token, { httpOnly: true })
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
